f06df4b95e8e0d38b712570b1c2543da1b43a3cb
Rumana <rumanaparven16@gmail.com>
2020-10-30 23:40:40 +0530

    first commit

--- Employee_Payroll.sql
+++ Employee_Payroll.sql
@@ -1,10 +1,13 @@
+--uc1 create database
 create table employee_payroll(
 
+--uc2 create employee payroll tablle
 id int NOT NULL identity(1,1) PRIMARY KEY,
 name VARCHAR(50) NOT NULL,
 salary DECIMAL(10,2) NOT NULL,
 start_Date DATETIME NOT NULL ) ;
 
+--uc3 insert values into the table
 insert into employee_payroll values
 (
 'Akash', 50000, '2020-10-18'
@@ -19,22 +22,21 @@ insert into employee_payroll values
 (
 'Rumana', 50000, '2018-10-18'
 )
+--uc4 retrieve data
+select * from employee_payroll
 
+--uc5 retrieve salary for particular employee
 select salary from employee_payroll where name='Sagar'
 select salary from employee_payroll where start_Date between cast('2018-01-01' as date) and GETDATE();
 
-select * from employee_payroll
 
-
+--uc6 add gender column and update rows
 alter table employee_payroll ADD gender CHAR(1)
-select * from employee_payroll
-
-
 update employee_payroll set gender='M' where name='Akash' or name='Sagar';
 update employee_payroll set gender='F' where name='Rumana' ;
 
-select * from employee_payroll
 
+--uc7 find sum avg min max etc for salary
 select sum(salary) from employee_payroll
 select sum(salary) from employee_payroll where gender='M' group by gender
 select avg(salary) from employee_payroll where gender='M' group by gender
@@ -42,9 +44,8 @@ select avg(salary) from employee_payroll where gender='F' group by gender
 select max(salary) from employee_payroll where gender='M' group by gender
 select count(name) from employee_payroll where gender='M'
 
+--uc8 expand employee payroll data
 alter table employee_payroll ADD employee_phone varchar(15),address varchar(100),department varchar(100) 
-
-select * from employee_payroll
 alter table employee_payroll alter column department varchar(100) not null;
 UPDATE employee_payroll 
 SET department = 0
@@ -55,6 +56,5 @@ UPDATE employee_payroll
 SET address = 'India'
 WHERE address IS NULL;
 
-select * from employee_payroll
-
+--uc9 expand employee payroll data
 alter table employee_payroll ADD basic_pay decimal(10,2),deductions decimal(10,2),taxable_pay decimal(10,2),income_tax decimal(10,2),net_pay decimal(10,2)
